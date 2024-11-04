23ea61eee0dde495d1148881d84fd308de577a18
Liam Nguyen <jimmynlnguyen@gmail.com>
2024-11-04 21:32:57 +0000

    Completed dom-event

---
+++ dom-events/buttons.ts
@@ -0,0 +1,37 @@
+const $click = document.querySelector('.click-button');
+if (!$click) {
+  throw new Error('Something is wrong!!!');
+}
+$click.addEventListener('click', () => {});
+
+function handleClick(event: any): any {
+  console.log('button clicked');
+  console.log('event', event);
+  console.log('event.target', event.target);
+}
+
+$click.addEventListener('click', handleClick);
+
+const $hover = document.querySelector('.hover-button');
+if (!$hover) {
+  throw new Error('Something is wrong');
+}
+$hover.addEventListener('hover', () => {});
+
+function handleMouseover(event: any): any {
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+$hover.addEventListener('hover', handleMouseover);
+
+const $double = document.querySelector('.double-click-button');
+if (!$double) throw new Error('Something is wrong');
+$double.addEventListener('click', () => {});
+
+function handleDoubleClick(event: any): any {
+  console.log('hover hovered');
+  console.log(event);
+  console.log(event.target);
+}
+$hover.addEventListener('Double-Click', handleDoubleClick);
