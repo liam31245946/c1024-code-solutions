8dc9d501e1f540294f9463c2e0e823d058f5dfc7
Liam Nguyen <jimmynlnguyen@gmail.com>
2024-11-08 22:18:03 +0000

    completed typescript-view-swapping

---
+++ typescript-view-swapping/main.js
@@ -0,0 +1,35 @@
+'use strict';
+const $tabContainer = document.querySelector('.tab-container');
+const $tab = document.querySelectorAll('.tab');
+const $view = document.querySelectorAll('.view');
+if (!$tabContainer) throw new Error('$tabContainer query failed');
+if (!$tab) throw new Error('$tab query failed');
+if (!$view) throw new Error('$view query failed');
+function click(event) {
+  const $eventTarget = event.target;
+  if ($eventTarget.matches('.tab')) {
+    console.log('.tab is match');
+    for (let i = 0; i < $tab.length; i++) {
+      const tab = $tab[i];
+      if (tab === $eventTarget) {
+        tab.classList.add('active');
+      } else {
+        tab.classList.remove('active');
+      }
+    }
+  } else {
+    console.log('.tab does not match');
+  }
+  const $data = $eventTarget.getAttribute('data-view');
+  if (!$data) throw new Error('$data query failed, gud luck fixing that');
+  for (let x = 0; x < $view.length; x++) {
+    const view = $view[x];
+    const viewData = view.getAttribute('data-view');
+    if (viewData === $data) {
+      view.classList.remove('hidden');
+    } else {
+      view.classList.add('hidden');
+    }
+  }
+}
+$tabContainer.addEventListener('click', click);
