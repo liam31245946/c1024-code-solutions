1a6fa85dc9e89d8e39a5b0b08bdae680db0cd548
Liam Nguyen <jimmynlnguyen@gmail.com>
2024-12-09 22:24:13 +0000

    typescript-modules

---
+++ typescript-modules/my-app/src/Body.tsx
@@ -0,0 +1,13 @@
+import BottomTextSection from './BottomTextSection';
+import MidTextSection from './MidTextSection';
+import TitleSection from './TitleSection';
+
+export default function Body() {
+  return (
+    <>
+      <TitleSection />
+      <MidTextSection />
+      <BottomTextSection />
+    </>
+  );
+}
---
+++ typescript-modules/my-app/src/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <h4>Modules are fun!! Copyright LearningFuze 2024</h4>;
+}
